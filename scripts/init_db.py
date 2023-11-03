77cf942025cd77bbf6b1bb6cc86305ecbf43c850
Noelle Marshall <noelle.marshall17@gmail.com>
2023-11-03 11:40:48 -0600

    Configuration and set up of endpoint tests

--- scripts/init_db.py
+++ scripts/init_db.py
@@ -1,16 +1,24 @@
 #!python
+from pathlib import Path
+
 import libsql_client as libsql
 import sys
 import os
 
 
 def main():
+    BASE_DIR = Path(__file__).resolve().parent.parent
+
+    force_rebuild = False
     if len(sys.argv) > 1:
         db_file = sys.argv[1]
     else:
         db_file = "./recipes.db"
 
-    if os.path.isfile(db_file):
+    if len(sys.argv) > 2:
+        force_rebuild = True if (sys.argv[2] == "--force") else False
+
+    if os.path.isfile(db_file) and not force_rebuild:
         yn = input(f"found existing db: '{db_file}', delete and rebuild this db? y/n ")
         if yn.lower() == "n" or yn.lower() == "no":
             print("Pass in a file path to create db at a different location")
@@ -20,7 +28,7 @@ def main():
 
     statements = None
     # todo: should this be dynamic?
-    with open("./scripts/schema.sql") as f:
+    with open(BASE_DIR.joinpath("./scripts/schema.sql")) as f:
         statements = [libsql.Statement(stat) for stat in f.read().split(';')]
 
     print(f"creating db at '{db_file}' ...")
---
+++ tests/PostEndpointTest.py
@@ -0,0 +1,35 @@
+from conftest import client
+
+
+def testCreatePost(client):
+    response = client.post("/post", json=dict(id="id", author="author", groupId="groupId",
+                                              content="content", dateCreated="dateCreated", likes="likes",
+                                              recipeId="recipeId", tags="tags"))
+    assert response.status_code == 200
+    assert "id" in response.get_json()["data"]
+
+
+def testGetSinglePost(client):
+    postId = "id"
+    author = "author"
+    groupId = "groupId"
+    content = "content"
+    recipeId = "recipeId"
+    tags = "tags"
+    response = client.post("/post", json=dict(id=postId, author=author, groupId=groupId,
+                                              content=content,
+                                              recipeId=recipeId, tags=tags))
+
+    postId = response.get_json()['data']['id']
+    response = client.get(f"/post/id/{postId}")
+    assert response.status_code == 200
+
+    post = response.get_json()['data']['post']
+    assert post['id'] == postId
+    assert post['author'] == author
+    assert post['groupId'] == groupId
+    assert post['content'] == content
+    assert post['dateCreated'] is not None
+    assert post['likes'] == 0
+    assert post['recipeId'] == recipeId
+    assert post['tags'] == tags

ee84bbfa610bf7c120a5155514874d4edc61318d
Noelle Marshall <noelle.marshall17@gmail.com>
2023-11-03 11:41:55 -0600

    Fixed type and replaced deprecated function

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ app.logger.setLevel(logging.INFO)
 if load_dotenv(os.getenv("ENV_PATH")):
     app.logger.info(f"Loaded env from path: {os.getenv('ENV_PATH')}")
 else:
-    app.logger.warn(f"Unable to load env from path: {os.getenv('ENV_PATH')}")
+    app.logger.warning(f"Unable to load env from path: {os.getenv('ENV_PATH')}")
 
 
 loglevel = logging.INFO
@@ -28,7 +28,7 @@ if os.getenv("LOG_LEVEL"):
         case "warn":
             loglevel = logging.WARN
         case "error":
-            loglevel = logging.ERRO
+            loglevel = logging.ERROR
         case "critical":
             loglevel = logging.CRITICAL
         case _:
